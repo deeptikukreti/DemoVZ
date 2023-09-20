69078c03e9322c8c824639bf43965f034679b9e0
sachidanand.pandit <spandit.sp11@gmail.com>
2023-09-20 18:19:30 +0530

    WorkManager imp

--- app/src/main/java/com/example/demovz/GroupDetailActivity.kt
+++ app/src/main/java/com/example/demovz/GroupDetailActivity.kt
@@ -7,38 +7,69 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
+import androidx.work.Constraints
+import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.demovz.adapter.DevicesListAdapter
 import com.example.demovz.databinding.ActivityGroupDetailBinding
+import com.example.demovz.workManager.NotificationWorker
 
-class GroupDetailActivity : AppCompatActivity() , DevicesListAdapter.OnItemClickListener{
-    var binding : ActivityGroupDetailBinding?=null
-    var deviceList =ArrayList<String>()
+class GroupDetailActivity : AppCompatActivity(), DevicesListAdapter.OnItemClickListener {
+    var binding: ActivityGroupDetailBinding? = null
+    var deviceList = ArrayList<String>()
     lateinit var deviceAdapter: DevicesListAdapter
+    private val workManager by lazy {
+        WorkManager.getInstance(applicationContext)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityGroupDetailBinding.inflate(layoutInflater)
+        binding = ActivityGroupDetailBinding.inflate(layoutInflater)
         setContentView(binding?.root)
         val name = intent.extras?.getString("GROUP_NAME")
-        binding?.txtGroupName?.text=name
-        deviceList.addAll(arrayOf("TV","Led Lights"))
-        deviceAdapter = DevicesListAdapter(deviceList).apply{ setOnClickListener(this@GroupDetailActivity) }
-        binding?.rvGrp?.adapter=deviceAdapter
+        binding?.txtGroupName?.text = name
+        deviceList.addAll(arrayOf("TV", "Led Lights"))
+        deviceAdapter =
+            DevicesListAdapter(deviceList).apply { setOnClickListener(this@GroupDetailActivity) }
+        binding?.rvGrp?.adapter = deviceAdapter
 
         binding?.addBtn?.setOnClickListener {
             createAlertDialog()
         }
+
+
+    }
+
+    private fun createOneTimeWorkRequest() {
+        // 1
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresStorageNotLow(true)
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val imageWorker = OneTimeWorkRequestBuilder<NotificationWorker>()
+            .setConstraints(constraints)
+            .addTag("noticeWork")
+            .build()
+        // 2
+        workManager.enqueueUniqueWork(
+            "oneTimeImageDownload",
+            ExistingWorkPolicy.KEEP,
+            imageWorker
+        )
     }
 
 
     @SuppressLint("MissingInflatedId")
-    fun createAlertDialog(){
-        val builder = AlertDialog.Builder(this,R.style.CustomAlertDialog)
+    fun createAlertDialog() {
+        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog)
             .create()
-        val view = layoutInflater.inflate(R.layout.add_device_layout,null)
-        val  device_name = view.findViewById<EditText>(R.id.edt_device)
-        val  save = view.findViewById<Button>(R.id.btn_save)
-        val  cancel = view.findViewById<Button>(R.id.btn_cancel)
+        val view = layoutInflater.inflate(R.layout.add_device_layout, null)
+        val device_name = view.findViewById<EditText>(R.id.edt_device)
+        val save = view.findViewById<Button>(R.id.btn_save)
+        val cancel = view.findViewById<Button>(R.id.btn_cancel)
         builder.setView(view)
         save.setOnClickListener {
             deviceList.add(device_name.text.toString())
@@ -52,8 +83,8 @@ class GroupDetailActivity : AppCompatActivity() , DevicesListAdapter.OnItemClick
         builder.show()
     }
 
-    override fun onClicked(s:String) {
-        startActivity(Intent(this,DeviceDetailActivity::class.java).putExtra("DEVICE_NAME",s))
+    override fun onClicked(s: String) {
+        startActivity(Intent(this, DeviceDetailActivity::class.java).putExtra("DEVICE_NAME", s))
     }
 
     override fun onDeviceRemoved(position: Int) {
---
+++ app/src/main/java/com/example/demovz/workManager/NotificationWorker.kt
@@ -0,0 +1,54 @@
+package com.example.demovz.workManager
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.demovz.MainActivity
+import com.example.demovz.R
+
+class NotificationWorker(private val context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
+    override fun doWork(): Result {
+        val intent = Intent(context, MainActivity::class.java)
+
+        // FLAG_UPDATE_CURRENT specifies that if a previous
+        // PendingIntent already exists, then the current one
+        // will update it with the latest intent
+        // 0 is the request code, using it later with the
+        // same method again will get back the same pending
+        // intent for future reference
+        // intent passed here is to our afterNotification class
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // RemoteViews are used to use the content of
+        // some different layout apart from the current activity layout
+        val contentView = RemoteViews(packageName, R.layout.activity_after_notification)
+
+        // checking if android version is greater than oreo(API 26) or not
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this, channelId)
+                .setContent(contentView)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                .setContentIntent(pendingIntent)
+        } else {
+
+            builder = Notification.Builder(this)
+                .setContent(contentView)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234, builder.build())
+        return Result.success()
+    }
+}
