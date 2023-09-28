bbc3f418c4a75ef8843930c9458f1afa806ec0e4
sachidanand.pandit <spandit.sp11@gmail.com>
2023-09-28 16:36:10 +0530

    Implemented event end date UI added DB.

---
+++ app/src/main/java/com/example/demovz/ui/review_settings/ReviewSettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.demovz.ui.review_settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.demovz.R
+import com.example.demovz.databinding.ActivityMainBinding
+import com.example.demovz.databinding.ActivityReviewSettingsBinding
+
+class ReviewSettingsActivity : AppCompatActivity() {
+    private var binding: ActivityReviewSettingsBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityReviewSettingsBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.apply {
+            appBar.txtTitle.text= getString(R.string.review_settings)
+        }
+
+    }
+}
